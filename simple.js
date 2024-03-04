ee0a4bda4f6c566910ec7290ef391f45ecbfd071
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-03-04 15:33:42 -0500

    Realizando un segundo post

--- simple.js
+++ simple.js
@@ -9,10 +9,19 @@ db.defaults({posts: []}).write()
 
 // add post
 // ----------------------------
-db.get('posts')
-  .push({ id: 1, title: 'lowdb is awesome', published: true})
+
+
+  db.get('posts')
+  .push({ id: 2, title: 'great', published: true})
   .write()
 
+  db.get('posts')
+  .push({ id: 3, title: 'new own', published: true})
+  .write()
+
+  db.get('posts')
+  .push({ id: 4, title: 'random', published: true})
+  .write()
 // count posts
 // ----------------------------
 // YOUR CODE
