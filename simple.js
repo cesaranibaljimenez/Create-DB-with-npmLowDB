ddb14f34ac57525bd7643135407252101925d9be
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-03-04 16:42:57 -0500

    Encontrando todos los posts no publicados

--- simple.js
+++ simple.js
@@ -39,8 +39,19 @@ db.defaults({posts: []}).write();
 
 // all matches of published false
 // ----------------------------
-var unPublishedPost = db.get('posts').filter({published : false}).value(); 
-console.log("Post no publicados:", unPublishedPost);
+//var unPublishedPost = db.get('posts').filter({published : false}).value(); 
+//console.log("Post no publicados:", unPublishedPost);
+
+
 // find post with published false
 // ----------------------------
-// YOUR CODE
+// // Encuentra todas las publicaciones donde el campo 'published' es false
+var unpublishedPosts = db.get('posts').filter({ published: false }).value();
+
+// Extrae el título y el ID de cada publicación no publicada
+var unpublishedTitlesAndIds = unpublishedPosts.map(post => {
+    return { id: post.id, title: post.title };
+});
+
+console.log("Títulos e IDs de todas las publicaciones no publicadas:", unpublishedTitlesAndIds);
+
